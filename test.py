90645be8ec572577a9e182a92d1787eb31d64227
ssglob <edwardlaw21@hotmail.com>
2024-09-23 16:05:10 -0700

    added test.py to gitignore

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-def f(x):
-    return x
-print(f(1,2))
+r = '    '
+n = int(input('Up to what number '))
+for i in range(n):
+    r += f' {i+1:3}'
+r += f'\n{'-'*len(r)}'
+for i in range(1,n+1):
+    r += '\n'
+    r += f'{i:2} |'
+    for ii in range(1,n+1):
+        r += f' {i*ii:3}'
+print(r)
