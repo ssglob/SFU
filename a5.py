877a0688236afde36c5034ed82a720b675d32bf7
ssglob <edwardlaw21@hotmail.com>
2024-11-18 12:46:36 -0800

    Fixed all bugs in a5, did styling things as well

--- a5.py
+++ a5.py
@@ -16,8 +16,10 @@
 
 #
 # import random for random turn order every round
+# import time so that each print is not instant
 #
 import random
+import time
 
 #
 # Define a function to compare two rolls
@@ -26,16 +28,16 @@ def compare(roll1,roll2):
     #
     # Conditions: roll1 rank == roll2 rank
     #
-    roll_vals = [100, 2, 3, 4, 5, 60]
+    roll_vals = [100, 2, 3, 4, 5, 60] # index for number's value is number-1
     if roll1==roll2: # If they are tied (456 and 123 included)
         return 't'
 
-    elif roll1.count(roll1[0]) == 3: # Both rolls are three in a row
+    elif roll1.count(roll1[0])==3 and roll2.count(roll2[0])==3: # Both rolls are three in a row
         if roll1[0]>roll2[0]:
             return '1'
         else:
             return '2'
-    else: # if both highest score and cur_score == 1
+    else: # if both highest rank and current rank == 1
         dice_sum1 = sum([roll_vals[i-1] for i in roll1])
         dice_sum2 = sum([roll_vals[i-1] for i in roll2])
         if dice_sum1==dice_sum2:
@@ -53,7 +55,7 @@ def humanturn(max_rolls,prev_rolls):
     rolls_used = 0
     #
     # Only need to print out the dice for humanturn, so make them here
-    # since dice rolls will be numbers can make it a list, and index would be
+    # Since dice rolls will be numbers make it a list, and index would be
     # roll-1
     #
     dice = '''
@@ -61,10 +63,10 @@ def humanturn(max_rolls,prev_rolls):
 |     |   |     |   |     |
 |     |   |     |   |     |
 |     |   |     |   |     |
-+-----+   +-----+   +-----+'''.strip() #Just add the o character to each dice
-    dice = [list(i) for i in dice.split('\n')] # make the dice easier to edit
++-----+   +-----+   +-----+'''.strip() # Just add the o characters to each dice
+    dice = [list(i) for i in dice.split('\n')] # Make the dice easier to edit
     #
-    # for the coordinates of o, define a list and the index for the correct dice is roll-1
+    # For the coordinates of o, define a list and the index for the correct dice is roll-1
     #
     dice_coords = [
         [[2,3]], [[1,2],[3,4]], [[1,2],[2,3],[3,4]], [[1,2],[1,4],[3,2],[3,4]], 
@@ -113,11 +115,12 @@ def humanturn(max_rolls,prev_rolls):
             while not yesorno in 'yn':
                 print("Invalid input. Try again. Enter y for yes and n for no.")
                 yesorno = input('Roll again? y/n: ')
-                print()
 
             if yesorno == 'n':
                 roll_again == False 
                 break
+    time.sleep(1.0)
+    print()
     rolls.sort() #Return sorted rolls because it's easier to work with
     return [rolls,rolls_used]
 
@@ -127,9 +130,10 @@ def humanturn(max_rolls,prev_rolls):
 #
 def robotturn(max_rolls,Lowest_roll):
     '''CPU should try to roll until they beat the lowest roll, because the most
-    important thing is to not lose. It does not matter if they have the highest score.'''
+    important thing is to not lose. It does not matter if they have the highest rank.
+    If it's first then it should try to roll until it gets a Loco, a three-of-a-kind,
+    or a Poco'''
     rolls_used = 0
-    rolls = [] # Using a list, we can easily store the result of each roll
 
     while rolls_used < max_rolls:
         rolls_used += 1
@@ -137,17 +141,22 @@ def robotturn(max_rolls,Lowest_roll):
         rolls.sort() # sort rolls because it's easier to work with
         if rolls == [4,5,6]:
             break
-        score = 1
+        rank = 1
         if rolls == [1,2,3]:
-            score = 2
+            rank = 2
         elif rolls.count(rolls[0]) == 3:
-            score = 3
+            rank = 3
         if Lowest_roll:
-            if Lowest_roll[0]<score:
+            if Lowest_roll[0]<rank:
                 break
-            elif Lowest_roll[0]==score:
+            elif Lowest_roll[0]==rank:
                 if compare(Lowest_roll[1],rolls):
                     break
+        else:
+            if rolls in [[1,2,3],[4,5,6]]: # Poco or Loco
+                break
+            elif rolls.count(rolls[0]) == 3: # Three of a kind
+                break
 
     return [rolls,rolls_used] 
 
@@ -170,29 +179,45 @@ def game():
     # Miscellaneous tasks (ask for name, print title, instructions)
     #
     username = input("What is your name? ")
+    chipnum = input("How many chips do you want everyone to start with? ")
+    while not chipnum.isdigit():
+        print("Please enter a positive integer.")
+        input("How many chips do you want everyone to start with? ")
+    chipnum = int(chipnum)
+
+    print()
     print('PocoLoco\n')
-    print('''Instructions: PocoLoco is played in rounds.  
-        In each round, players take turns rolling three dice and try to get the 
-        highest score possible. After all the players have taken their turn in a round, 
-        the lowest-scoring player is given chips from the other players as described below.
-        The game ends when a player loses all their chips. 
-        The first player with zero chips is the winner.
+    time.sleep(1.0)
+    print('''Instructions: 
+PocoLoco is played in rounds.  
+In each round, players take turns rolling three dice and try to get the 
+highest score possible. After all the players have taken their turn in 
+a round, the lowest-scoring player is given chips from the other players 
+as described below.
+
+Lowest-scoring player receives:
+1 chip if the winners score is a points total
+2 chips if the winners score is 1, 2, 3 (Loco!)
+3 chips if the winners score is any three-of-a-kind
+4 chips if the winners score is 4, 5, 6 (PoCo!)\n
         
-        Lowest-scoring player receives:
-        1 chip if the winners score is a points total
-        2 chips if the winners score is 1, 2, 3 (Loco!)
-        3 chips if the winners score is any three-of-a-kind
-        4 chips if the winners score is 4, 5, 6 (PoCo!)\n
-          
-        If there is a tie, the winner and loser are randomly selected.
-        ''')
+The game ends when a player loses all their chips. 
+The first player with zero chips is the winner.
+    
+If there is a tie, the winner and loser are randomly selected.
+''')
     #
-    # define a dictionary that holds the chips of each player
-    # the computers are p# where # is an integer greater than 1
-    # player is username
-    # define another dictionary for special rolls, another one for roll values
+    # Ask for an input to pause until they finish reading the instructions
     #
-    chips = {username: 10, 'p2': 10, 'p3': 10, 'p4': 10}
+    input("Enter any key to continue when you are ready. ")
+    print()
+    #
+    # Define a dictionary that holds the chips of each player
+    # The computers are p# where # is an integer greater than 1
+    # Player is username
+    # Define another dictionary for special rolls, another one for roll values
+    #
+    chips = {username: chipnum, 'p2': chipnum, 'p3': chipnum, 'p4': chipnum}
 
     #
     # start rounds
@@ -201,32 +226,50 @@ def game():
     cur_round = 1
     while not game_over:
         #
-        # print and update current round
+        # Print and update current round
         #
         print(printround(cur_round))
+        print()
         cur_round += 1
+        time.sleep(1.0)
 
         #
-        # print player names and their respective chips
+        # Print player names and their respective chips
         #
         print('Current chips:')
         for item in chips:
             print(f'{item}: {chips[item]} chips, ')
+        print()
 
+        #
+        # Define game variables
+        #
         players = list(chips.keys())
         round_rolls = {}
         max_rolls = 3
         prev_rolls = []
-        Highest_roll = [] # [score, hand, player]
-        Lowest_roll = [] # [score, hand, player]
-        HTied = [] # list of all rolls tied with highest roll, clear after new highest roll
-        LTied = [] # lis tall rolls tied with with Lowest roll, clear after new lowest roll
+        Highest_roll = [] # [rank, hand, player]
+        Lowest_roll = [] # [rank, hand, player]
+        HTied = [] # List of all rolls tied with highest roll, clear after new highest roll
+        LTied = [] # List of all rolls tied with with Lowest roll, clear after new lowest roll
 
-        for n in range(4):
-            cur_player = players.pop(random.randint(0,3-n))
+        #
+        # Choose order of the players
+        #
+        order = [players.pop(random.randint(0,3-n)) for n in range(4)]
+        print('Order for this round:')
+        #
+        # print the order
+        #
+        for count,item in enumerate(order):
+            print(f'{count+1}: {item}')
+        print()
+        time.sleep(2.0)
 
+        for cur_player in order:
             if cur_player == username:
-                round_rolls[cur_player],numofrolls = humanturn(max_rolls,list(round_rolls.values()))
+                player_turn = humanturn(max_rolls,list(round_rolls.values()))
+                round_rolls[cur_player],numofrolls = player_turn
 
             else:
                 round_rolls[cur_player],numofrolls = robotturn(max_rolls,Lowest_roll)
@@ -234,64 +277,59 @@ def game():
             prev_rolls.append(round_rolls[cur_player])
 
             #
-            # Calculate Player score
+            # Calculate the roll's rank
             #
             cur_roll = round_rolls[cur_player]
 
             if cur_roll == [4,5,6]:
-                player_score = 4
-            elif cur_roll.count(cur_roll[0]):
-                player_score = 3
+                player_rank = 4
+            elif cur_roll.count(cur_roll[0]) == 3:
+                player_rank = 3
             elif cur_roll == [1,2,3]:
-                player_score = 2
+                player_rank = 2
             else:
-                player_score = 1
+                player_rank = 1
 
             #
             # Update Lowest and highest roll
             #
-            if not Highest_roll or not Lowest_roll:
-                Lowest_roll = [player_score,cur_roll,cur_player]
-                Highest_roll = [player_score,cur_roll,cur_player]
+            if not Highest_roll or not Lowest_roll: # First turn
+                Lowest_roll = [player_rank,cur_roll,cur_player]
+                Highest_roll = [player_rank,cur_roll,cur_player]
                 HTied.append(cur_player)
                 LTied.append(cur_player)
-            elif player_score>Highest_roll[0]:
-                Highest_roll = [player_score,cur_roll,cur_player]
-                HTied.clear()
-                HTied.append(cur_player)
-            elif player_score<Lowest_roll[0]:
-                Lowest_roll = [player_score,cur_roll,cur_player]
-                LTied.clear()
-                LTied.append(cur_player)
+            elif player_rank>Highest_roll[0]:
+                Highest_roll = [player_rank,cur_roll,cur_player]
+                HTied = [cur_player]
+            elif player_rank<Lowest_roll[0]:
+                Lowest_roll = [player_rank,cur_roll,cur_player]
+                LTied = [cur_player]
             
             #
             # use compare function to compare the rolls
             #
-            elif player_score==Highest_roll[0]:
-                higher = compare(Highest_roll[1],cur_roll)
-                if higher == 't':
-                    HTied.append(cur_player)
-                elif higher == '1':
-                    continue
-                elif higher == '2':
-                    Highest_roll = [player_score,cur_roll,cur_player]
-                    HTied.clear()
-                    HTied.append(cur_player)
+            else:
+                if player_rank==Highest_roll[0]:
+                    higher = compare(Highest_roll[1],cur_roll)
+                    if higher == 't':
+                        HTied.append(cur_player)
+                    elif higher == '2':
+                        Highest_roll = [player_rank,cur_roll,cur_player]
+                        HTied = [cur_player]
 
-            elif player_score==Lowest_roll[0]:
-                lower = compare(Lowest_roll[1],cur_roll)
-                if lower == 't':
-                    LTied.append(cur_player)
-                elif lower == '1':
-                    Lowest_roll = [player_score,cur_roll,cur_player]
-                    LTied.clear()
-                    LTied.append(cur_player)
-                elif lower == '2':
-                    continue
+                if player_rank==Lowest_roll[0]:
+                    lower = compare(Lowest_roll[1],cur_roll)
+                    if lower == 't':
+                        LTied.append(cur_player)
+                    elif lower == '1':
+                        Lowest_roll = [player_rank,cur_roll,cur_player]
+                        LTied = [cur_player]
             
         #
         # decide winner/loser and calculate chips
         #
+        print(LTied)
+        print(HTied)
         winner = HTied[random.randint(0,len(HTied)-1)] # choose random winner if there is a tie
         loser = LTied[random.randint(0,len(LTied)-1)] # same as choosing winner
 
@@ -304,8 +342,19 @@ def game():
                 chips[key] = 0
                 game_over = True
         
+        print('Rolls from this round (sorted):')
+        for key in chips:
+            print(f'{key}: {round_rolls[key]}')
+        print()
+        
         print(f'The winner of this round was {winner}. The loser of this round was {loser}.')
         print()
+        #
+        # ask for input so the user decides when to continues to the next round
+        #
+        input("Enter any key to continue to the next round. ")
+        print()
+
     highest_chips = 0 # loser
     winners = []
     losers = []
@@ -319,9 +368,11 @@ def game():
             losers.append(key)
         elif chips[key]==highest_chips:
             losers.append(key)
-        
+    
+    print('Game over.')
+    print()
     print(f'''Winner of the game was {winners[random.randint(0,len(winners)-1)]}
-            Loser of the game was {losers[random.randint(0,len(losers)-1)]}''')
+Loser of the game was {losers[random.randint(0,len(losers)-1)]}''')
     print(f'\nFinal chips of each player:')
 
     for key in chips:
