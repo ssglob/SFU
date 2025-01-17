f2122e10ee4ac3e68c33071cf363dd0c74e8e693
ssglob <edwardlaw21@hotmail.com>
2025-01-17 14:42:25 -0800

    Removed extra files and started Assignment 1

---
+++ CMPT 125/temperature.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    float T_start = -10.2;
+    float T_room = 21.8;
+    printf("enter the number of full hours since the power failed: ");
+    int hours;
+    scanf("%d",&hours);
+    printf("enter the number of additional minutes since the power failed: ");
+    int minutes;
+    scanf("%d",&minutes);
+    float power_off = minutes/60.0 + hours/1.0;
+    float cur_T = T_start/1.0 + 0.05 * power_off * (T_room-T_start);
+    printf("The power has been off for %0.2lf hours\n",power_off);
+    printf("Estimated present temperature in freezer is %0.2lf degrees Celsius",cur_T);
+}
